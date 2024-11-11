ca0e87696bd195ba93823f679cb3a826e1cf0ae6
Wagner Gonçalves Pinto <73096490+wagnifico@users.noreply.github.com>
2024-11-11 18:34:33 +0100

    define the complete case based on the incompressible/simpleFoam/airFoil2D tutorial

--- case/run.sh
+++ case/run.sh
@@ -3,10 +3,16 @@
 #cd "${0%/*}" || exit
 . ${WM_PROJECT_DIR:?}/bin/tools/RunFunctions
 
+restore0Dir
+
+
 runApplication blockMesh
 runApplication topoSet
 # runApplication foamToVTK -faceSet plateFaceSet # to check faceSet
-runApplication createBaffles
+runApplication createBaffles -overwrite
 runApplication checkMesh
+
+
+runApplication $(getApplication)
 
 paraFoam -touch-all
